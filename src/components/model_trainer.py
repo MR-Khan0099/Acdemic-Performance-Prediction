b09e12667ebcc3cef023d8cfe430ee3495df9201
MR-KHAN0099 <mohammedrizwan0909@gmail.com>
2024-09-04 23:29:24 +0200

    Added Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,17 +1,90 @@
-# Basic Import
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt 
-import seaborn as sns
-# Modelling
-from sklearn.metrics import mean_squared_error, r2_score
+import os
+import sys
+import logging
+from dataclasses import dataclass
+from src.exception import CustomException
+
+from catboost import CatBoostRegressor
+
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
-from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression, Ridge,Lasso
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-from sklearn.model_selection import RandomizedSearchCV
-from catboost import CatBoostRegressor
-from xgboost import XGBRegressor
-import warnings
+from xgboost import XGBClassifier, XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import evaluate_models, save_object
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+
+    def initiate_model_trainer(self, train_array, test_array):
+
+        try:
+            logging.info("Splitting the data into train and test arrays")
+            x_train,y_train,x_test,y_test = (train_array[::-1],#take out last and feed it to the model
+                                             train_array[:,-1],#
+                                             test_array[::-1],
+                                             test_array[:,-1]
+            )
+            
+            models = {
+                "RandomForest": RandomForestRegressor(),
+                "GradientBoosting": GradientBoostingRegressor(),
+                "AdaBoost classifier": AdaBoostRegressor(),
+                "XGBoost": XGBRegressor(),
+                "CatBoost": CatBoostRegressor(verbose=False),
+                "DecisionTree": DecisionTreeRegressor(),
+                "LinearRegression": LinearRegression(),
+                "k-Neighbors": KNeighborsRegressor(),
+                "XGBClassifier": XGBClassifier(),
+            }
+
+            model_report:dict= evaluate_models(x_train=x_train, y_train=y_train,x_test=x_test, y_test=y_test, 
+                                              models=models)
+            
+            # get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            # get best model name from dict
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+                
+            ]
+
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.7:
+                raise CustomException("No best model found", sys)
+
+
+            logging.info(f"Best found model on the basis of R2 score is: {best_model_name}")        
+
+        
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predictions = best_model.predict(x_test)
+
+            r2_score = r2_score(y_test, predictions)
+            return r2_score
+
+           
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,55 @@
+# import os
+# import sys
+# import numpy as np
+# import pandas as pd
+# import dill
+# from sklearn.metrics import r2_score
+# from src.exception import CustomException
+# from src.logger import logging
+
+
+
+# def save_object(obj, file_path):
+#     try:
+#         dir_path = os.path.dirname(file_path)
+#         os.makedirs(dir_path, exist_ok=True)
+
+#         with open(file_path, "wb") as file_obj:
+#             dill.dump(obj, file_obj) #dill library is used to make pkl file
+
+#     except Exception as e:
+#         raise CustomException(e, sys)
+    
+# def evaluate_models(models, x_train,y_train, x_test, y_test):
+#     report={}
+#     try:
+#         for i in range(len(list(models))):
+#             model = list(models.values())[i]
+
+#             model.fit(x_train, y_train)
+
+#             y_train_pred = model.predict(x_train)
+
+#             y_test_pred = model.predict(x_test)
+
+#             train_model_score = r2_score(y_train, y_train_pred)
+#             test_model_score = r2_score(y_test, y_test_pred)
+
+#             report.list(models.keys())[i] = {"train_score":train_model_score, "test_score":test_model_score}
+           
+        
+#     except Exception as e:
+#         logging.error("Error occurred while evaluating the model: ", e)
+#         raise CustomException(e, sys)
+
 import os
 import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 from src.exception import CustomException
-
-
+from src.logger import logging
 
 def save_object(obj, file_path):
     try:
@@ -13,7 +57,31 @@ def save_object(obj, file_path):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj) #dill library is used to make pkl file
+            dill.dump(obj, file_obj)  # dill library is used to make pkl file
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_models(models, x_train, y_train, x_test, y_test):
+    report = {}
+    try:
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(x_train, y_train)
+
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            # Correct assignment to the report dictionary
+            model_name = list(models.keys())[i]
+            report[model_name] = {"train_score": train_model_score, "test_score": test_model_score}
+        
+    except Exception as e:
+        logging.error("Error occurred while evaluating the model: ", e)
+        raise CustomException(e, sys)
+
+    return report
