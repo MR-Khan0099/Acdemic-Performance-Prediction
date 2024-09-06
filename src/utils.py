5b3acf55e974e05857786b34016330e42c0f62ea
MR-KHAN0099 <mohammedrizwan0909@gmail.com>
2024-09-06 12:20:21 +0200

    Modified model trainer

--- src/utils.py
+++ src/utils.py
@@ -1,87 +1,62 @@
-# import os
-# import sys
-# import numpy as np
-# import pandas as pd
-# import dill
-# from sklearn.metrics import r2_score
-# from src.exception import CustomException
-# from src.logger import logging
-
-
-
-# def save_object(obj, file_path):
-#     try:
-#         dir_path = os.path.dirname(file_path)
-#         os.makedirs(dir_path, exist_ok=True)
-
-#         with open(file_path, "wb") as file_obj:
-#             dill.dump(obj, file_obj) #dill library is used to make pkl file
-
-#     except Exception as e:
-#         raise CustomException(e, sys)
-    
-# def evaluate_models(models, x_train,y_train, x_test, y_test):
-#     report={}
-#     try:
-#         for i in range(len(list(models))):
-#             model = list(models.values())[i]
-
-#             model.fit(x_train, y_train)
-
-#             y_train_pred = model.predict(x_train)
-
-#             y_test_pred = model.predict(x_test)
-
-#             train_model_score = r2_score(y_train, y_train_pred)
-#             test_model_score = r2_score(y_test, y_test_pred)
-
-#             report.list(models.keys())[i] = {"train_score":train_model_score, "test_score":test_model_score}
-           
-        
-#     except Exception as e:
-#         logging.error("Error occurred while evaluating the model: ", e)
-#         raise CustomException(e, sys)
-
 import os
 import sys
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
-from src.exception import CustomException
-from src.logger import logging
+from sklearn.model_selection import GridSearchCV
 
-def save_object(obj, file_path):
+from src.exception import CustomException
+
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)  # dill library is used to make pkl file
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(models, x_train, y_train, x_test, y_test):
-    report = {}
+def evaluate_models(X_train, y_train,X_test,y_test,models):
     try:
+        report = {}
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            # para=param[list(models.keys())[i]]
 
-            model.fit(x_train, y_train)
+            # gs = GridSearchCV(model,para,cv=3)
+            # gs.fit(X_train,y_train)
 
-            y_train_pred = model.predict(x_train)
-            y_test_pred = model.predict(x_test)
+            # model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            # train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
 
-            # Correct assignment to the report dictionary
-            model_name = list(models.keys())[i]
-            report[model_name] = {"train_score": train_model_score, "test_score": test_model_score}
-        
-    except Exception as e:
-        logging.error("Error occurred while evaluating the model: ", e)
-        raise CustomException(e, sys)
+            report[list(models.keys())[i]] = test_model_score
 
-    return report
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
